5d5ccf331a197c4fb8a3933de6542bd3cb460c71
clux <sszynrae@gmail.com>
2021-11-28 21:58:23 +0000

    release -fixes #2
    Signed-off-by: clux <sszynrae@gmail.com>

--- stats.sh
+++ stats.sh
@@ -3,5 +3,5 @@ set -e
 
 players=( Guy Florent April Jay Richard Svein Verity Rhys Paul Shaun Spike James )
 for p in "${players[@]}"; do
-    echo "$(rg Players: -A 6 recaps/ | grep -E "\- $p" | sort -n | wc -l) $p"
+    echo "$(rg Players: -A 6 docs/recaps/ | grep -E "\- $p" | sort -n | wc -l) $p"
 done | sort -r -n
