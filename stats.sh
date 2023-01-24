29d70ae6a4ec1fd783b1c29893f8aa752d0c4cb9
clux <sszynrae@gmail.com>
2023-01-24 04:55:51 +0000

    word
    Signed-off-by: clux <sszynrae@gmail.com>

--- stats.sh
+++ stats.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-# reduce players vector in recaps, and count occurrences found across all files
+# reduce players vector in recaps, and count player occurrences found across all files
 rg Players: -A 6 docs/recaps/ | rg "\- (\w*)" -r '$1' -o | sort | uniq -c | sort -hr
