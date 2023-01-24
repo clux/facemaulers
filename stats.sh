95977c3b9d2d8927a522c6886b0b1764b7ac9392
clux <sszynrae@gmail.com>
2023-01-24 04:53:37 +0000

    simplify stats script
    Signed-off-by: clux <sszynrae@gmail.com>

--- stats.sh
+++ stats.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-players=( Guy Florent April Jay Richard Svein Verity Rhys Paul Shaun Spike James )
-for p in "${players[@]}"; do
-    echo "$(rg Players: -A 6 docs/recaps/ | grep -E "\- $p" | sort -n | wc -l) $p"
-done | sort -r -n
+rg Players: -A 6 docs/recaps/ | rg "\- (\w*)" -r '$1' -o | sort | uniq -c | sort -hr
